60ff31aa0686a5c7666fdbbd9cfd0cc04719318a
gmg <minggang0829@gmail.com>
2018-09-11 13:47:53 +0800

    commit

---
+++ Matplotlib/line.py
@@ -0,0 +1,3 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [3, 6, 9], '-r')
+plt.show()
--- Matplotlib/matplotlibDemo.py
+++
@@ -1,5 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-data=np.arange(100, 201)
-plt.plot(data)
-plt.show()
