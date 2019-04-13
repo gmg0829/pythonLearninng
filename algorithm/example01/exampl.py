ff053dae00c575ff302e0407020294545e8b1aba
gmg0829 <minggang0829@gmail.com>
2019-04-13 18:02:18 +0800

    commit

---
+++ algorithm/example01/exampl.py
@@ -0,0 +1,20 @@
+def seq_search(items: list, elem) -> int:
+    """顺序查找"""
+    for index, item in enumerate(items):
+        if elem == item:
+            return index
+    return -1
+
+
+def bin_search(items, elem):
+    """二分查找"""
+    start, end = 0, len(items) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if elem > items[mid]:
+            start = mid + 1
+        elif elem < items[mid]:
+            end = mid - 1
+        else:
+            return mid
+    return -1
