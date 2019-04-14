d38a1f8b1beaecd7113df39ea4c8b80adad04ec8
gmg0829 <minggang0829@gmail.com>
2019-04-14 17:31:16 +0800

    commit

---
+++ scikit-learn/CoreMLModelDemo.py
@@ -0,0 +1,9 @@
+
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+
+full_data = pd.read_csv("Data.csv")
+model = LinearRegression()
+model.fit(full_data[['Gender', 'Height']], full_data["Weight"])
+
+print(model.predict([[1, 168]]))
