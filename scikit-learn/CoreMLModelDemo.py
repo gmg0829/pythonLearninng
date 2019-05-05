9f9bdf78f433a74b00984418d63ee3b50befc17b
gmg0829 <minggang0829@gmail.com>
2019-05-05 17:45:13 +0800

    commit

--- scikit-learn/CoreMLModelDemo.py
+++ scikit-learn/CoreMLModelDemo.py
@@ -5,5 +5,4 @@ import pandas as pd
 full_data = pd.read_csv("Data.csv")
 model = LinearRegression()
 model.fit(full_data[['Gender', 'Height']], full_data["Weight"])
-
 print(model.predict([[1, 168]]))
